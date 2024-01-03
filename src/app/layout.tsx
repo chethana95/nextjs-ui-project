57822b6b2cc80af2a8bc544543c5830b8d2192a0
chethana95 <chethana.virajini@upview.tech>
2024-01-03 12:19:07 +0530

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next'
 
 import './globals.css'
-import Navbar from '@/components/Navbar'
-import Footer from '@/components/Footer'
+import Navbar from '@/components/navbar/Navbar'
+import Footer from '@/components/footer/Footer'
 
 
 export const metadata: Metadata = {
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,14 +1,14 @@
 import { NAV_LINKS } from "@/constants"
 import Image from "next/image"
 import Link from "next/link"
-import Button from "./Button"
+import Button from "../buttons/Button"
 
 
 const Navbar = () => {
   return (
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
-      <Link href={'/'}>
-        <Image src={'/hilink-logo.svg'} alt="logo" width={74} height={29} />
+      <Link href='/'>
+        <Image src='/hilink-logo.svg' alt="logo" width={74} height={29} />
       </Link>
       <ul className="hidden h-full gap-12 lg:flex">
         {NAV_LINKS.map((link) => (
@@ -26,7 +26,7 @@ const Navbar = () => {
         />
       </div>
       <Image
-        src={'menu.svg'}
+        src='menu.svg'
         alt="menu"
         width={32}
         height={32}
